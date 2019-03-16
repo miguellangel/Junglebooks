9895a524272760b7bce2362587eca0799e0164e7
miguel__ <arriaga.miguel123@gmail.com>
2019-03-16 01:40:11 -0500

    basic horizontal navigation bar + styling closes #9

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/book/')
-def web_page2():
-    return render_template('web_page2.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == "__main__":
     app.run()
 #----------------------------------------
