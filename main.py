22fd481f77f44c9de3b4cb68d3bb8a383faf4721
miguel__ <arriaga.miguel123@gmail.com>
2019-03-22 00:19:55 -0500

    Implemented bootstrap features (nav-bar and grid), updated the home page.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 #-----------------------------------------
-# main2.py: creating first flask application
+# main.py: handles project webpage requests
 #-----------------------------------------
 from flask import Flask, render_template
+import json
+
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template("index.html")
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/statistics')
+def statistics():
+	return render_template('statistics.html')
+
 if __name__ == "__main__":
     app.run()
 #----------------------------------------
-# end of main2.py
+# end of main.py
 #-----------------------------------------
