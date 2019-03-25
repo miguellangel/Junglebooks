f10d1c917ef6c75c330c4c40fbd1ce458c2bd7aa
musty-j <40575672+musty-j@users.noreply.github.com>
2019-03-24 21:45:52 -0500

    linking all pages together

--- main.py
+++ main.py
@@ -8,6 +8,41 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	return render_template("index.html")
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/statistics')
+def statistics():
+	return render_template("statistics.html")
+
+@app.route('/book1')
+def book1():
+	return render_template("book_1.html")
+	
+@app.route('/book2')
+def book2():
+	return render_template("book_2.html")
+	
+@app.route('/book3')
+def book3():
+	return render_template("book_3.html")
+	
+@app.route('/book1/author')
+def jkrowling():
+	return render_template("jkrowling.html")
+
+@app.route('/book2/author1')
+def rjordan():
+	return render_template("rjordan.html")
+
+@app.route('/book2/author2')
+def bsanderson():
+	return render_template("bsanderson.html")
+
+@app.route('/book3/author')
+def jrrtolkein():
+	return render_template("jrrtolkein.html")
 
 if __name__ == "__main__":
     app.run()
