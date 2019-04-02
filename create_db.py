79cbbedaacc8110d3e10d69bc13727d0e07ec4db
musty-j <40575672+musty-j@users.noreply.github.com>
2019-04-01 22:40:05 -0500

    creating database. issues with creating book.json

--- create_db.py
+++ create_db.py
@@ -1,6 +1,6 @@
 # import and open the json file
 import json
-from models import app, db, Book
+from models import app, db, Book, Publisher, Author
 
 def load_json(filename):
     with open(filename) as file:
@@ -12,68 +12,73 @@ def load_json(filename):
 # extract and format the book data
 def create_books():
     book = load_json('books.json')
-
-    for oneBook in book['Books']:
-        title = oneBook['title']
-        google_id = oneBook['google_id']
-        isbn = oneBook['isbn']
-        if isbn == None:
-            isbn = google_id
-        date = oneBook['publication_date']
-        image = oneBook['image_url']
-        description = oneBook['description']
-
+    list_key = ['title', 'google_id', 'isbn','publication_date','image_url','description']
+    
+    for oneBook in book['Books']:		
+        newKeylist = []
+        keys_list = oneBook.keys()
+        for key in list_key :
+          if key in keys_list:
+            key = oneBook[key]
+            newKeylist.append(key)
+          else:
+            key = None
         # create a new book object utilizing the formatted data
-        newBook = Book(title = title, google_id = google_id, isbn = isbn, date = date, image = image, description = description)
+        print(newKeylist)
+        newBook = Book(title = newKeylist[0], google_id = newKeylist[1], isbn = newKeylist[2], date = newKeylist[3], image = newKeylist[4], description = newKeylist[5])
         
         # After I create the book, I can then add it to my session. 
         db.session.add(newBook)
         # commit the session to my DB.
         db.session.commit()
-
+    print("done")
 # extract and format the publisher data
 def create_pub():
     book = load_json('books.json')
-
+    pub_num = 1
     for oneBook in book['Books']:
         onePub = oneBook['publishers'][0]
         pub_name = onePub['name']
+        pub_id = pub_name + ' ' +str(pub_num)
         wiki_url = onePub['wikipedia_url']
         pub_description = onePub['description']
         pub_owner = onePub['owner']
         pub_image = onePub['image_url']
         pub_website = onePub['website']
-
+        pub_num += 1
         # create a new publisher object utilizing the formatted data
-        newPub = Publisher(name = name, wiki_url = wiki_url, pub_description = pub_description, pub_owner = pub_owner, pub_image = pub_image, pub_website = pub_website)
+        newPub = Publisher(pub_id = pub_id, pub_name = pub_name, wiki_url = wiki_url, pub_description = pub_description, pub_owner = pub_owner, pub_image = pub_image, pub_website = pub_website)
 
         # After I create the publisher, I can then add it to my session
         db.session.add(newPub)
         # commit the session to my DB
         db.session.commit()
-
+    print('done')
+		
 # create and format the author data
 def create_authors():
     book = load_json('books.json')
-
+    author_num = 1
     for oneBook in book['Books']:
         oneAuthor = oneBook['authors'][0]
         name = oneAuthor['name']
+        author_id = name +' '+ str(author_num)
         born = oneAuthor['born']
-        education = oneAuhtor['education']
+        education = oneAuthor['education']
         nationality = oneAuthor['nationality']
         description = oneAuthor['description']
         alma_mater = oneAuthor['alma_mater']
         wiki_url = oneAuthor['wikipedia_url']
         image = oneAuthor['image_url']
-
+        author_num += 1
         # create a new author object utilizing the formatted data
-        newAuthor = Author(name = name, born = born, education = education, nationality = nationality, description = description, alma_mater = alma_mater, wiki_url = wiki_url, image = image)
+        newAuthor = Author(author_id = author_id, name = name, born = born, education = education, nationality = nationality, description = description, alma_mater = alma_mater, wiki_url = wiki_url, image = image)
 
         # After I create the publisher, I can then add it to my session
-        db.session.add(newPub)
+        db.session.add(newAuthor)
         # commit the session to my DB
-        de.session.commit()
+        db.session.commit()
+    print('done')
 
 
 create_books()
--- models.py
+++ models.py
@@ -4,13 +4,41 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",'postgres://postgres:idbg20@localhost:5432/book_db1')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_STRING",'postgres://postgres:idbg20@localhost:5432/book_db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # to suppress a warning message
 db = SQLAlchemy(app)
+
 class Book(db.Model):
  __tablename__ = 'book'
 
  title = db.Column(db.String(80), nullable = False)
- id = db.Column(db.Integer, primary_key = True)
+ google_id = db.Column(db.String(80), primary_key = True)
+ isbn = db.Column(db.BigInteger, nullable = True)
+ date = db.Column(db.String(80), nullable = True)
+ image = db.Column(db.String(80), nullable = True)
+ description = db.Column(db.String(), nullable = True)
+ 
+class Publisher(db.Model):
+ __tablename__ = 'publisher'
+ pub_id = db.Column(db.String(80), primary_key = True)
+ pub_name = db.Column(db.String(80), nullable = False)
+ wiki_url = db.Column(db.String(), nullable = False)
+ pub_description = db.Column(db.String(), nullable = False)
+ pub_owner = db.Column(db.String(), nullable = False)
+ pub_image = db.Column(db.String(), nullable = False)
+ pub_website = db.Column(db.String(), nullable = False)
+
+class Author(db.Model):
+ __tablename__ = 'author'
+ author_id = db.Column(db.String(80), primary_key = True)
+ name = db.Column(db.String(80), nullable = False)
+ born = db.Column(db.String(), nullable = False)
+ education = db.Column(db.String(), nullable = False)
+ nationality = db.Column(db.String(), nullable = False)
+ description = db.Column(db.String(), nullable = False)
+ alma_mater = db.Column(db.String(), nullable = False)
+ wiki_url = db.Column(db.String(), nullable = False)
+ image = db.Column(db.String(), nullable = False)
+
 db.create_all()
 # End of models.py 
